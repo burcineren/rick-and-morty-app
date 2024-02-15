6984b414af57ecc9441c54be0ca374aa0d2b7042
burcineren <burcin.ern969@gmail.com>
2024-02-15 22:58:45 +0300

    feature: characters filter

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -9,23 +9,40 @@ import { Character } from "../model/character.model";
   styleUrls: ["./characters.component.scss"],
 })
 export class CharactersComponent {
-  character:  Character[] = [];
+  characters: Character[] = [];
   filteredCharacters: Character[] = [];
-  filterOptions = { name: '', species: '', status: '', gender: '' };
+  filterOptions = { name: "", species: "", status: "", gender: "" };
+  statusOptions: string[] = ["Alive", "Dead", "Unknown"];
+  genderOptions: string[] = ["Male", "Female", "Genderless", "Unknown"];
+
   constructor(private baseService: BaseService) {
-    this.baseService
-      .getCharacter()
-      .subscribe((res: any) => (this.character = res.results));
+    this.baseService.getCharacter().subscribe((res: any) => {
+      this.characters = res.results;
+      this.filteredCharacters = [...this.characters];
+    });
   }
+
   filterCharacters(): void {
-    this.filteredCharacters = this.character.filter(character => {
+    this.filteredCharacters = this.characters.filter((character) => {
       return (
-        character.name.toLowerCase().includes(this.filterOptions.name.toLowerCase()) &&
-        character.species.toLowerCase().includes(this.filterOptions.species.toLowerCase()) &&
-        character.status.toLowerCase().includes(this.filterOptions.status.toLowerCase()) &&
-        character.gender.toLowerCase().includes(this.filterOptions.gender.toLowerCase())
+        character.name
+          .toLowerCase()
+          .includes(this.filterOptions.name.toLowerCase()) &&
+        character.species
+          .toLowerCase()
+          .includes(this.filterOptions.species.toLowerCase()) &&
+        character.status
+          .toLowerCase()
+          .includes(this.filterOptions.status.toLowerCase()) &&
+        character.gender
+          .toLowerCase()
+          .includes(this.filterOptions.gender.toLowerCase())
       );
     });
   }
-  
+
+  clearFilters(): void {
+    this.filterOptions = { name: "", species: "", status: "", gender: "" };
+    this.filterCharacters();
+  }
 }
